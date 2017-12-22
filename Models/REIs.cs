0cdc3b893cfa4f8620c58af18cce25f6d0ca267c
William Gross <grossw@seattleu.edu>
2017-12-22 10:39:56 -0800

    Initial commit

---
+++ Models/REIs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTML5_Sandbox.Models
+{
+    public class REI
+    {
+        public string name;
+        public int chassis;
+    }
+}
